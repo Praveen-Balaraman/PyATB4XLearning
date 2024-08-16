0164e755c4e59c0a421cdde7e96d35abc273009a
Praveen Balaraman <praveenravi6@gmail.com>
2024-08-16 23:59:58 +0530

    init

---
+++ SRC/EX_13_08_2024/Lab007.py
@@ -0,0 +1,9 @@
+# Take two inputs and perform sum, sub and div
+num1=input("Enter number1")
+num2=input("Enter number2")
+sum=(int(num1)+int(num2))
+sub=(int(num1)-int(num2))
+mul=(int(num1)*int(num2))
+div=(int(num1)/int(num2))
+
+print(sum,sub,mul,div)
---
+++ SRC/EX_13_08_2024/Lab009.py
@@ -0,0 +1,3 @@
+number=3.14444456
+formatted_number=f"{number:.4f}"
+print(formatted_number)
