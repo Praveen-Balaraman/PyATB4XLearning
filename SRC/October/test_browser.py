eaef9b07a5d94472169e1230f38991ab0f728d5f
Praveen Balaraman <praveenravi6@gmail.com>
2024-11-29 09:28:36 +0530

    Task 8 and 9  added

--- SRC/October/test_browser.py
+++ SRC/October/test_browser.py
@@ -1,8 +1,12 @@
 import pytest
 import time
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
+
+@allure.epic("VWO Login Test")
+@allure.feature("TC#0 - VWO App Negative Test")
 def test_browser():
  driver.get("https://practicetestautomation.com/practice-test-login/")
  driver.maximize_window()
@@ -13,3 +17,4 @@ def test_browser():
  driver.find_element(By.ID,"submit").click()
  time.sleep(5)
  assert 1+1==2
+
